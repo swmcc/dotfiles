c95e11cf6ba848d5b28e42bbf48a9b427bf12726
Stephen McCullough <stephen@sixdegreelabs.com>
2015-04-21 12:58:34 +0100

    #1 - Added in getting oh-my-zsh and changing to zsh as well as adding aliases

--- bootstraph.sh
+++ bootstraph.sh
@@ -4,6 +4,10 @@ OS=`uname`
 
 function os_install {
 	echo "OSX"
+	echo "Run the following commands if you need to."
+	echo "curl -L https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sh"
+  	echo "chsh -s /bin/zsh"
+	cp aliases/.aliases $HOME/.aliases
 }
 
 function main {
@@ -17,4 +21,3 @@ function main {
 main
 
 # need to install base utils.. 
-# aliases
