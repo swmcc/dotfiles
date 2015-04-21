f6e843a55612991050935110ace2fd5af1ee4914
Stephen McCullough <stephen@sixdegreelabs.com>
2015-04-21 12:49:05 +0100

    #1 - Started the bootstrap script. Will add to it as I move things onto the work laptop

--- bootstraph.sh
+++ bootstraph.sh
@@ -1,3 +1,20 @@
 #!/usr/bin/env bash
 
-cd "$(dirname "${BASH_SOURCE}")";
+OS=`uname`
+
+function os_install {
+	echo "OSX"
+}
+
+function main {
+	case "$OS" in
+	  Darwin)   os_install ;; 
+	  linux*)   echo "LINUX" ;;
+	  *)        echo "unknown: $OSTYPE" ;;
+	esac
+}
+
+main
+
+# need to install base utils.. 
+# aliases
