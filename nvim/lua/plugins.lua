c11aa74b5348e02643afbf4d1f820b67ba7cffef
Stephen McCullough <stephen@skillfulgorilla.com>
2024-07-01 15:02:55 +0100

    ♻️ - Updated the plugins configuration in `plugins.lua`
    - Removed several unused and added `vim-fugitive` and `vim-test`.
    - Added configuration for `vim-test`.

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -1,75 +1,18 @@
-return require('packer').startup(function(use)
-  -- Packer can manage itself
-  use 'wbthomason/packer.nvim'
-
-  -- Lazy loading:
-  -- Load on specific commands
-  use {'tpope/vim-dispatch', opt = true, cmd = {'Dispatch', 'Make', 'Focus', 'Start'}}
-
-  -- Load on an autocommand event
-  use {'andymass/vim-matchup', event = 'VimEnter'}
-
-  -- Load on a combination of conditions: specific filetypes or commands
-  -- Also run code after load (see the "config" key)
-  use {
-    'w0rp/ale',
-    ft = {'sh', 'zsh', 'bash', 'c', 'cpp', 'cmake', 'html', 'markdown', 'racket', 'vim', 'tex'},
-    cmd = 'ALEEnable',
-    config = 'vim.cmd[[ALEEnable]]'
-  }
-
-  -- Plugins can have dependencies on other plugins
-  use {
-    'haorenW1025/completion-nvim',
-    opt = true,
-    requires = {{'hrsh7th/vim-vsnip', opt = true}, {'hrsh7th/vim-vsnip-integ', opt = true}}
-  }
-
-  -- Plugins can have post-install/update hooks
-  use {'iamcco/markdown-preview.nvim', run = 'cd app && yarn install', cmd = 'MarkdownPreview'}
-
-  -- Post-install/update hook with neovim command
-  use { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' }
-
-  -- Post-install/update hook with call of vimscript function with argument
-  use { 'glacambre/firenvim', run = function() vim.fn['firenvim#install'](0) end }
-
-  -- Use specific branch, dependency and run lua file after load
-
-  -- Use dependency and run lua function after load
-  use {
-    'lewis6991/gitsigns.nvim', requires = { 'nvim-lua/plenary.nvim' },
-    config = function() require('gitsigns').setup() end
-  }
-
-  -- You can alias plugin names
-  use {'dracula/vim', as = 'dracula'}
-
-	-- The above is from an example config file. Need to tidy it up, but this will get me started.
-  use {
-    'nvim-telescope/telescope.nvim', tag = '0.1.0',
-    requires = { {'nvim-lua/plenary.nvim'} }
-  }
-
-  use 'neovim/nvim-lspconfig' -- Configurations for Nvim LSP
-  
-  use {
-    "startup-nvim/startup.nvim",
-    requires = {"nvim-lua/plenary.nvim"},
+return {
+  -- Other plugins...
+  {
+    "tpope/vim-fugitive",
+    "vim-test/vim-test",
     config = function()
-      require("startup").setup { theme = "evil" }
+      -- Optional: Configure vim-test with key mappings
+      vim.cmd([[
+        let test#strategy = "neovim"
+        nnoremap <leader>tn :TestNearest<CR>
+        nnoremap <leader>tf :TestFile<CR>
+        nnoremap <leader>ta :TestSuite<CR>
+        nnoremap <leader>tl :TestLast<CR>
+        nnoremap <leader>tv :TestVisit<CR>
+      ]])
     end,
-  }
-
-  use {
-      'numToStr/Comment.nvim',
-      config = function()
-          require('Comment').setup()
-      end
-  }
-
-  use {
-      'nvim-lualine/lualine.nvim',
-      requires = { 'kyazdani42/nvim-web-devicons', opt = true }
-  }
-end)
+  },
+}
