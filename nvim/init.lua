b7cf44fcb27f535edc92984c1b0b0c11a4235196
Stephen McCullough <stephen@skillfulgorilla.com>
2024-07-15 13:59:50 +0100

    ✨ - Added rg to nvim

--- nvim/init.lua
+++ nvim/init.lua
@@ -5,8 +5,37 @@ vim.opt.swapfile = false
 
 require("telescope").setup({
 	defaults = {
+		prompt_prefix = "> ",
+		selection_caret = "> ",
+		entry_prefix = "  ",
+		initial_mode = "insert",
+		selection_strategy = "reset",
+		sorting_strategy = "descending",
+		layout_strategy = "horizontal",
+		layout_config = {
+			horizontal = {
+				mirror = false,
+			},
+			vertical = {
+				mirror = false,
+			},
+		},
+		file_sorter = require("telescope.sorters").get_fuzzy_file,
 		file_ignore_patterns = { "node_modules", ".git", "dist", "build", "tmp" },
+		generic_sorter = require("telescope.sorters").get_generic_fuzzy_sorter,
+		path_display = { "absolute" },
+		winblend = 0,
+		border = {},
+		color_devicons = true,
+		use_less = true,
+		set_env = { ["COLORTERM"] = "truecolor" },
+		file_previewer = require("telescope.previewers").vim_buffer_cat.new,
+		grep_previewer = require("telescope.previewers").vim_buffer_vimgrep.new,
+		qflist_previewer = require("telescope.previewers").vim_buffer_qflist.new,
+		buffer_previewer_maker = require("telescope.previewers").buffer_previewer_maker,
 	},
+	pickers = {},
+	extensions = {},
 })
 
 vim.api.nvim_set_keymap("i", "<Tab>", 'copilot#Accept("<Tab>")', { expr = true, noremap = true })
