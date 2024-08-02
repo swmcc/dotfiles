fb4a7648fc6cf5d2233484d4137804428ff7718e
Stephen McCullough <stephen@skillfulgorilla.com>
2024-08-02 23:45:17 +0100

    ♻️ - Added vendor to the ignore path for telescope

--- nvim/init.lua
+++ nvim/init.lua
@@ -271,7 +271,7 @@ require("telescope").setup({
 			},
 		},
 		file_sorter = require("telescope.sorters").get_fuzzy_file,
-		file_ignore_patterns = { "node_modules", ".git", "dist", "build", "tmp" },
+		file_ignore_patterns = { "node_modules", ".git", "vendor", "dist", "build", "tmp" },
 		generic_sorter = require("telescope.sorters").get_generic_fuzzy_sorter,
 		path_display = { "absolute" },
 		winblend = 0,
